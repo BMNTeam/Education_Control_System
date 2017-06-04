6ec08dedaa64290461cd772ca1a0f45204ba0b1b
BMNTeam <maksim_bender08@mail.ru>
2017-06-04 19:46:13 +0300

    Start work with user Authentication

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var bcrypt = require('bcrypt');
+
+var saltRounds = 12;
+var someSyntaxSalt = 'BMNTeamSaltHaHa4562ai23$!@%!@35@#%rasDFsadfZXv2345';
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.post('/', function (req, res, next) {
+    //Google reCaptcha checking
+
+    // Save request to a variables
+    var subject = req.body.subject;
+    var pwd = req.body.pwd;
+    var email = req.belongsTo.email;
+
+    //Save data to database
+    models.User.findOrCreate({
+        subject: subject,
+        role: 'teacher',
+        pwd: bcrypt.genSalt(saltRounds, (err, salt) => {
+            bcrypt.hash(pwd, someSyntaxSalt, (err, hash) => {
+                //some callback;
+            })
+        }),
+        email: email,
+        rememberToken: ''
+    }).then( response => {
+        res.json(response);
+    })
 });
 
 module.exports = router;
